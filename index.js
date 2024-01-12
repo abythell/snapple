beb3f412c6bc2db2e38d2d4aa1c92cf3c35c8ac1
Andrew Bythell <abythell@ziptrek.com>
2024-01-12 15:42:11 -0800

    add initial lcd support

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const SnapMeta = require('./lib/snap-meta.js')
+const Raspi = require('rasp-io').RaspiIO
+const j5 = require('johnny-five')
+const LCD = require('./lib/lcd.js')
+require('dontenv').config // eslint-disable-line no-unused-expressions
 
-const snap = new SnapMeta('192.168.0.2')
+/**
+ * Initialize Hardware
+ */
+let lcd
+const board = new j5.Board({ io: new Raspi(), repl: false })
+board.on('ready', () => {
+  lcd = new LCD(j5)
+})
+
+/**
+ * Configure Snapcast
+ */
+const snap = new SnapMeta(process.env.SNAPSERVER)
 snap.on('error', console.error)
-snap.on('data', console.log)
+snap.on('data', (metadata) => {
+  lcd.title(metadata.title)
+})
+snap.on('status', lcd.status)
 snap.open().then(() => {
-  console.log('Opened control client')
+  lcd.title('Starting')
 }).catch(console.err)
 
 process.on('SIGINT', () => {
   snap.close().then(() => {
-    console.log('Closed control client')
+    process.exit(0)
   }).catch(console.err)
 })
