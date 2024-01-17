aff0173bd08d27ff6a53f067b2e2bb1b9e356866
Andrew Bythell <abythell@ieee.org>
2024-01-16 16:56:23 -0800

    save work

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const SnapMeta = require('./lib/snap-meta.js')
-const Raspi = require('rasp-io').RaspiIO
+const Raspi = require('raspi-io').RaspiIO
 const j5 = require('johnny-five')
 const LCD = require('./lib/lcd.js')
-require('dontenv').config // eslint-disable-line no-unused-expressions
 
-/**
- * Initialize Hardware
- */
-let lcd
+require('dotenv').config // eslint-disable-line no-unused-expressions
 const board = new j5.Board({ io: new Raspi(), repl: false })
+
 board.on('ready', () => {
-  lcd = new LCD(j5)
-})
+  const lcd = new LCD(j5)
+  const snap = new SnapMeta(process.env.SNAPSERVER)
 
-/**
- * Configure Snapcast
- */
-const snap = new SnapMeta(process.env.SNAPSERVER)
-snap.on('error', console.error)
-snap.on('data', (metadata) => {
-  lcd.title(metadata.title)
-})
-snap.on('status', lcd.status)
-snap.open().then(() => {
-  lcd.title('Starting')
-}).catch(console.err)
+  snap.on('error', console.error)
 
-process.on('SIGINT', () => {
-  snap.close().then(() => {
-    process.exit(0)
+  snap.on('data', (metadata) => {
+    lcd.title(metadata.title)
+  })
+
+  snap.on('status', lcd.setStatus)
+
+  snap.open().then(() => {
+    lcd.setTitle('Starting')
   }).catch(console.err)
+
+  process.on('SIGINT', () => {
+    snap.close().then(() => {
+      process.exit(0)
+    }).catch(console.err)
+  })
 })
+
+
